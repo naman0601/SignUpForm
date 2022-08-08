c686f7f158f635811891f7e638f752678ab80f71
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-08-09 00:55:54 +0530

    Update modified.php

--- modified.php
+++ modified.php
@@ -1,36 +1,62 @@
 <?php    
+
+   include "connection.php"; 
+   include "listing.php";
+
+while($row = mysqli_fetch_object($result)){    
     
-include "connection.php";    
     
-if(isset($_GET['id'])){    
-$sql = "delete from registration where id = '".$_GET['id']."'";    
-$result = mysql_query($sql);    
-}    
-    
-$sql = "select * from registration";    
-$result = mysql_query($sql);    
-?>    
-<html>    
-    <body>    
-        <table width = "100%" border = "1" cellspacing = "1" cellpadding = "1">    
-            <tr>    
-                <td>Id</td>    
-                <td>First Name</td>    
-                <td>Middle Name</td>    
-                <td>Last Name</td>    
-                <td>Password</td>    
-                <td>Confirm Password</td>    
-                <td>Email</td>    
-                <td>Contact No.</td>    
-                <td>Gender</td>    
-                <td>Address</td>    
-                <td>Pincode</td>    
-                <td>City</td>    
-                <td>Country</td>    
-                <td>Skills</td>    
-                <td>Files</td>    
-                <td colspan = "2">Action</td>    
-            </tr>    
-        </table>    
-    </body>    
-</html>  
+?>  
+    <tr>  
+        <td>  
+            <?php echo $row->id;?>  
+        </td>  
+        <td>  
+            <?php echo $row->fname;?>  
+        </td>  
+        <td>  
+            <?php echo $row->mname;?>  
+        </td>  
+        <td>  
+            <?php echo $row->lname;?>  
+        </td>  
+        <td>  
+            <?php echo $row->pwd;?>  
+        </td>  
+        <td>  
+            <?php echo $row->cnf;?>  
+        </td>  
+        <td>  
+            <?php echo $row->mail;?>  
+        </td>  
+        <td>  
+            <?php echo $row->number;?>  
+        </td>  
+        <td>G  
+            <?php echo $row->sex;?>  
+        </td>  
+        <td>  
+            <?php echo $row->address;?>  
+        </td>  
+        <td>  
+            <?php echo $row->code;?>  
+        </td>  
+        <td>  
+            <?php echo $row->city;?>  
+        </td>  
+        <td>  
+            <?php echo $row->country;?>  
+        </td>  
+        <td>  
+            <?php echo $row->skills;?>  
+        </td>  
+        <td>  
+            <?php echo $row->attach_file;?>  
+        </td>  
+        <td> <a href="listing.php?id =     
+            <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Delete    
+        </a> | <a href="index.php?id =     
+            <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Edit    
+        </a> </td>  
+        <tr>  
+            <?php } ?> 
